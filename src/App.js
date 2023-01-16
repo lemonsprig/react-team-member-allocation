43eb7c74526a169c4995087946f27b1240d3c1e0
Lemonsprig <ianselbyuk@gmail.com>
2023-01-16 17:04:36 +0000

    Functionality added and persisted to local storage.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,129 @@
+import './App.css'
 import Header from './Header';
 import Employees from './Employees';
 import Footer from './Footer';
+import { useState, useEffect } from "react";
 
 function App() {
+
+  const [selectedTeam, setTeam] = useState((localStorage.getItem('selectedTeam')) || "TeamA");
+  
+  const [employees, setEmployees] = useState(JSON.parse(localStorage.getItem('employeeList')) || [{
+    id: 1,
+    fullName: "Bob Jones",
+    designation: "JavaScript Developer",
+    gender: "male",
+    teamName: "TeamA"
+  },
+  {
+    id: 2,
+    fullName: "Jill Bailey",
+    designation: "Node Developer",
+    gender: "female",
+    teamName: "TeamA"
+  },
+  {
+    id: 3,
+    fullName: "Gail Shepherd",
+    designation: "Java Developer",
+    gender: "female",
+    teamName: "TeamA"
+  },
+  {
+    id: 4,
+    fullName: "Sam Reynolds",
+    designation: "React Developer",
+    gender: "male",
+    teamName: "TeamB"
+  },
+  {
+    id: 5,
+    fullName: "David Henry",
+    designation: "DotNet Developer",
+    gender: "male",
+    teamName: "TeamB"
+  },
+  {
+    id: 6,
+    fullName: "Sarah Blake",
+    designation: "SQL Server DBA",
+    gender: "female",
+    teamName: "TeamB"
+  },
+  {
+    id: 7,
+    fullName: "James Bennet",
+    designation: "Angular Developer",
+    gender: "male",
+    teamName: "TeamC"
+  },
+  {
+    id: 8,
+    fullName: "Jessica Faye",
+    designation: "API Developer",
+    gender: "female",
+    teamName: "TeamC"
+  },
+  {
+    id: 9,
+    fullName: "Lita Stone",
+    designation: "C++ Developer",
+    gender: "female",
+    teamName: "TeamC"
+  },
+  {
+    id: 10,
+    fullName: "Daniel Young",
+    designation: "Python Developer",
+    gender: "male",
+    teamName: "TeamD"
+  },
+  {
+    id: 11,
+    fullName: "Adrian Jacobs",
+    designation: "Vue Developer",
+    gender: "male",
+    teamName: "TeamD"
+  },
+  {
+    id: 12,
+    fullName: "Devin Monroe",
+    designation: "Graphic Designer",
+    gender: "male",
+    teamName: "TeamD"
+  }]);
+
+  useEffect(() => {
+    localStorage.setItem('employeeList', JSON.stringify(employees))
+  }, [employees]);
+
+  useEffect(() => {
+    localStorage.setItem('selectedTeam', selectedTeam)
+  }, [selectedTeam]);
+
+  function handleTeamSelectionChange(e) {
+    setTeam(e.target.value);
+  }
+  
+  function handleEmployeeCardClick(e) {
+    const transformedEmployees = employees.map((employee) => employee.id === parseInt(e.currentTarget.id)
+     ? (employee.teamName === selectedTeam)?{...employee, teamName:''}
+     : {...employee, teamName: selectedTeam}
+     :employee);
+     console.log(employees)
+     console.log(transformedEmployees)
+     setEmployees(transformedEmployees);
+  }
+
   return (
     <div className="App">
-      <Header/>
-      <Employees/>
+      <Header selectedTeam={selectedTeam}
+        teamMemberCount={employees.filter((employee) => employee.teamName === selectedTeam).length}/>
+      <Employees employees={employees} 
+        selectedTeam={selectedTeam}
+        handleEmployeeCardClick={handleEmployeeCardClick}
+        handleTeamSelectionChange={handleTeamSelectionChange}
+      />
       <Footer/>
     </div>
   );
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,14 @@
-const Header = () => {
+const Header = ({selectedTeam, teamMemberCount}) => {
 
   return (
-    <h1>Header</h1>
+    <header className="container">
+      <div className="row justify-content-center mt-3 mb-4">
+        <div className="col-8">
+          <h1>Team Member Allocation</h1>
+          <h3>{selectedTeam} as {teamMemberCount} Members</h3>
+        </div>
+      </div>
+    </header>
   )
 }
 
