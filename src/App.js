f5d479fce83a6e5f558b7ed7c9fb618344f3a610
Lemonsprig <ianselbyuk@gmail.com>
2023-01-16 10:24:59 +0000

    Header file created and liked to app.js. Header file loaded with dummy data array.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Header from './Header';
+import Employees from './Employees';
+import Footer from './Footer';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Header/>
+      <Employees/>
+      <Footer/>
     </div>
   );
 }
